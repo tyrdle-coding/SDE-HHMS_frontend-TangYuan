fb3d761b4c90c7367b53ca2d4c4fe78db3ef3e6c
2RoR2 <rachelcje@gmail.com>
2026-05-21 21:59:51 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,21 +9,14 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   root: path.resolve(__dirname),
-<<<<<<< HEAD
-
-=======
->>>>>>> 894e3de (update)
   plugins: [react(), tailwindcss()],
 
   server: {
     host: '0.0.0.0',
     port: 5173,
-<<<<<<< HEAD
-=======
     allowedHosts: [
       'sdehotelmanagementsystemtangyuan-production.up.railway.app',
     ],
->>>>>>> 894e3de (update)
     proxy: {
       '/api': 'http://localhost:3001',
     },
@@ -31,11 +24,7 @@ export default defineConfig({
 
   preview: {
     host: '0.0.0.0',
-<<<<<<< HEAD
     port: Number(process.env.PORT) || 8080,
-=======
-    port: 5173,
->>>>>>> 894e3de (update)
     allowedHosts: [
       'sdehotelmanagementsystemtangyuan-production.up.railway.app',
     ],
